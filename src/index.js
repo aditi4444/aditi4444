8d8d54ae01742067af86c0cd66ab680c1770c05a
aditi4444 <aditipurohit5@gmail.com>
2023-07-02 12:17:01 +0530

    dummy commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './Home';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
